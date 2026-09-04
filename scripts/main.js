508157691756261035979fc4e86c7e4e25c0ff0f
Dũng Ule (Nguyễn Trọng Dũng) <118347845+Dung-Ule@users.noreply.github.com>
2026-09-04 23:11:38 +0700

    new main.js commands!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 "use strict";
 
 alert("I'm a Taylor Swift's fan, please explore this website!");
-alert("Does it look good enough?");
+let answer = prompt("What do you think?", ["stunning"]);
+alert(`I think it looks ${answer}!`);
